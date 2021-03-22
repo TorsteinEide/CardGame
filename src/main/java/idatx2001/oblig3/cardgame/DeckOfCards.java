72ecf279cf8187a8e9062a62afef894c306f7b6b
Torstein <taurstein@gmail.com>
2021-03-22 12:54:44 +0100

    Added class DeckOfCards, created fields and constructor that initializes the full deck

---
+++ src/main/java/idatx2001/oblig3/cardgame/DeckOfCards.java
@@ -0,0 +1,26 @@
+package idatx2001.oblig3.cardgame;
+import java.util.ArrayList;
+
+/**
+ * This class represents a deck of cards
+ * where cards are "stored"
+ */
+public class DeckOfCards
+{
+    private final char[] suit = {'S', 'H', 'D', 'C' };
+    private final int[] rank = {1,2,3,4,5,6,7,8,9,10,11,12,13}; //1=ace, 11=jack, 12=queen, 13=king
+    private int[] deck = new int[52]; //Array that represents the full deck at 52 cards
+
+    /**
+     * Constructor initializes a object of class DeckOfCards
+     * Creates a deck of cards with 52 cards
+     */
+    public DeckOfCards()
+    {
+        for(int index = 0; index < deck.length; index++)
+        {
+            deck[index] = index;
+            System.out.println(index);
+        }
+    }
+}
